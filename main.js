6f8815b13a1c8bdf275ccdf493b8b93b8b1d928a
EvatheGreat <116327580+EvatheGreat@users.noreply.github.com>
2023-08-17 20:01:20 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 function preload(){
 }
 function setup(){
-    canvas=createCanvas(400,400);
+    canvas=createCanvas(500,400);
     canvas.center();
+    vid=createCapture(VIDEO);
+    vid.size(400,400);
+    vid.hide();
+    anam=ml5.poseNet(vid,modelLoaded);
+    anam.on('pose',gotPoses);
 }
 function draw(){
+    image(vid,0,0,500,400);
 }
 function snap(){
     save("FILTERZ 2.0.png");
+}
+function modelLoaded(){
+    console.log("I ❤️ CODING 💻");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
 }
